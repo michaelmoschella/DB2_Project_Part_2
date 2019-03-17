a1757271b54ce1d437ff7c0ebd819482df45f4d0
mmoschella <m_moschella@massbay.edu>
2019-03-17 13:16:19 -0400

    g

--- php/parent-login.php
+++ php/parent-login.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    $p_email = $_POST['Parent_Email_Login']; 
-    $p_pass = $_POST['Parent_Pass_Login']; 
+    $p_email = $_POST['Parent_Email_Login'];
+    $p_pass = $_POST['Parent_Pass_Login'];
 
-    $myconnection = mysqli_connect('localhost', 'root', '') 
+    $myconnection = mysqli_connect('localhost', 'root', '')
     or die ('Could not connect: ' . mysqli_error());
     $mydb = mysqli_select_db ($myconnection, 'DB2') or die ('Could not select database');
 
-    $get_pass_query = "SELECT password, username, uID FROM User WHERE email = \"{$p_email}\" AND uID IN 
+    $get_pass_query = "SELECT password, username, uID FROM User WHERE email = \"{$p_email}\" AND uID IN
     (SELECT pID FROM Parent);";
     $result1 = mysqli_query($myconnection, $get_pass_query) or die ('Query failed: ' . mysqli_error($myconnection));
     $row = mysqli_fetch_row($result1);
@@ -19,19 +19,17 @@
             echo("<h1>Welcome {$p_username}, you have successfully logged in!</h1>
                 <h3><a href='./parent-dashboard.php'>Click here to go to your parent dashboard</a></h3>
                 <h5><a href='./logout.php'>Logout</a></h5>");
-                
+
         } else {
             echo("<h1>Sorry, the provided password does not match the account for {$p_email}</h1>
-            <h3><a href='../useless.html'>Back to main page</a></h3>");
+            <h3><a href='../not_useless.html'>Back to main page</a></h3>");
         }
     } else {
         echo("<h1>Sorry, the email address {$p_email} is not registered to a parent in our Database</h1>
-            <h3><a href='../useless.html'>Back to main page</a></h3>");
+            <h3><a href='../not_useless.html'>Back to main page</a></h3>");
     }
     mysqli_free_result($result1);
 
     mysqli_close($myconnection);
     exit;
 ?>
-
-
--- php/student-login.php
+++ php/student-login.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    $s_email = $_POST['Student_Email_Login']; 
-    $s_pass = $_POST['Student_Pass_Login']; 
+    $s_email = $_POST['Student_Email_Login'];
+    $s_pass = $_POST['Student_Pass_Login'];
 
-    $myconnection = mysqli_connect('localhost', 'root', '') 
+    $myconnection = mysqli_connect('localhost', 'root', '')
     or die ('Could not connect: ' . mysqli_error());
     $mydb = mysqli_select_db ($myconnection, 'DB2') or die ('Could not select database');
 
-    $get_pass_query = "SELECT password, username, uID FROM User WHERE email = \"{$s_email}\" AND uID IN 
+    $get_pass_query = "SELECT password, username, uID FROM User WHERE email = \"{$s_email}\" AND uID IN
     (SELECT sID FROM Student);";
     $result1 = mysqli_query($myconnection, $get_pass_query) or die ('Query failed: ' . mysqli_error($myconnection));
     $row = mysqli_fetch_row($result1);
@@ -22,11 +22,11 @@
                 
         } else {
             echo("<h1>Sorry, the provided password does not match the account for {$s_email}</h1>
-            <h3><a href='../useless.html'>Back to main page</a></h3>");
+            <h3><a href='../not_useless.html'>Back to main page</a></h3>");
         }
     } else {
         echo("<h1>Sorry, the email address {$s_email} is not registered to a parent in our Database</h1>
-            <h3><a href='../useless.html'>Back to main page</a></h3>");
+            <h3><a href='../not_useless.html'>Back to main page</a></h3>");
     }
     mysqli_free_result($result1);
 
