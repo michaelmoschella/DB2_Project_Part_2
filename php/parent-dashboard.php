d2e96c1fd1a81a3eb6138b1a519611522bc08118
mmoschella <m_moschella@massbay.edu>
2019-03-19 16:54:43 -0400

    cc

--- php/parent-dashboard.php
+++ php/parent-dashboard.php
@@ -1,19 +1,20 @@
 <?php
     session_start();
-    
-    $myconnection = mysqli_connect('localhost', 'root', '') 
+
+    $myconnection = mysqli_connect('localhost', 'root', '')
     or die ('Could not connect: ' . mysqli_error());
     $mydb = mysqli_select_db ($myconnection, 'DB2') or die ('Could not select database');
-    
+
     $active_id = $_SESSION['active_ID'];
-    
+
     $get_info_query = "SELECT name, role FROM User WHERE {$active_id} = uID;";
     $result1 = mysqli_query($myconnection, $get_info_query) or die ('Query failed: ' . mysqli_error($myconnection));
     $row = mysqli_fetch_row($result1);
+    $p_role = $row[1];
     mysqli_free_result($result1);
-    
+
     echo('<h1>Your Dashboard</h1>');
-    
+
     $html_string = "<head>
         <style>
             table {
@@ -42,9 +43,9 @@
             <td>Parent</td>
             <td><a href='change-p-profile.php'>Change Your Profile</a></td>
         </tr>";
-        
-        
-    $get_children_query = "SELECT name, role, uid FROM User, Family WHERE Family.pID = {$active_id} AND Family.sID = User.uID;"; 
+
+
+    $get_children_query = "SELECT name, role, uid FROM User, Family WHERE Family.pID = {$active_id} AND Family.sID = User.uID;";
     $result2 = mysqli_query($myconnection, $get_children_query) or die ('Query failed: ' . mysqli_error($myconnection));
     while ($row = mysqli_fetch_row($result2)) {
         # pass childs uID through link
@@ -54,7 +55,21 @@
         <td><a href='change-c-profile.php?cID=".$row[2]."'>Change Your Child's Profile</a></td>
         </tr> ";
     }
-    
+
+    if($p_role == 'Moderator'){
+      $html_string .= "<tr>
+        <td>Parent</td>
+        <td>Section</td>
+        <td><a href=''>View Sections</a></td>
+      </tr>
+      <tr>
+        <td>Moderator</td>
+        <td>Moderator</td>
+        <td><a href=''>View Moderator</a></td>
+      </tr>
+      ";
+    }
+
     mysqli_free_result($result2);
     echo($html_string);
     echo('<h3><a href="logout.php">Logout</a></h3>');
