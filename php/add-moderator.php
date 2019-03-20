e6fb5a30c5b29132e73fcc05c59737b4fd84005b
mmoschella <m_moschella@massbay.edu>
2019-03-20 16:55:27 -0400

    ccc

---
+++ php/add-moderator.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+
+    $myconnection = mysqli_connect('localhost', 'root', '')
+    or die ('Could not connect: ' . mysqli_error());
+    $mydb = mysqli_select_db ($myconnection, 'DB2') or die ('Could not select database');
+
+    $active_id = $_SESSION['active_ID'];
+    $c_id = $_GET['c__ID'];
+    $sec_id = $_GET['sec__ID'];
+
+    echo("<h1>$c_id</h1>");
+
+    $get_student_info_query = "INSERT INTO Moderates VALUES ({$sec_id}, {$c_id}, {$active_id});";
+    $result2 = mysqli_query($myconnection, $get_student_info_query) or die ('Query failed: ' . mysqli_error($myconnection));
+
+    echo('<h1>Congratulations you are successfully added as a Moderator!</h1>');
+    echo('<h3><a href="parent-dashboard.php">Back to dashboard</a></h3>');
+    echo('<h3><a href="logout.php">Logout</a></h3>');
+
+    mysqli_close($myconnection);
+    exit;
+?>
--- php/parent-view-sections.php
+++ php/parent-view-sections.php
@@ -12,8 +12,8 @@ $active_id = $_SESSION['active_ID'];
     or die ('Could not connect: ' . mysqli_error());
     $mydb = mysqli_select_db ($myconnection, 'DB2') or die ('Could not select database');
 
-    $get_info_query = "SELECT secID, cID FROM Moderates WHERE {$active_id} = modID;";
-    $result0 = mysqli_query($myconnection, $get_info_query) or die ('Query failed: ' . mysqli_error($myconnection));
+
+
 
   #  $active_id = $_SESSION['active_ID'];
   #  $get_student_info_query = "SELECT grade, role FROM User, Student WHERE {$active_id} = uID AND {$active_id} = sID;";
@@ -96,19 +96,40 @@ $active_id = $_SESSION['active_ID'];
                     <td>0</td>
                     <td>0</td>";
                     if($parent_role=='Moderator'){
-                      $test = 0;
+
+
+                      $get_info_query = "SELECT secID, cID, modID FROM Moderates;";
+                      $result0 = mysqli_query($myconnection, $get_info_query) or die ('Query failed: ' . mysqli_error($myconnection));
+
                     while ($row0 = mysqli_fetch_row($result0)){
+                        $test = 0;
                       if($row0[0]==$row[11] && $row0[1]==$row[10]){
+                        if($row0[2]==$active_id){
                         $test = 1;
+                        break;
+
+                      } else {
+                        $test=2;
+                        break;
+
                       }
+
+                    }
                     }
                     if($test==0){
                       $html_string .= "
-                    <td><button onClick=''>Moderate</button>";
-                  } else {
+                    <td><form method='get' action='add-moderator.php'><input type='hidden' value='".$row[10]."' name='c__ID'>
+                    <button type='submit' value='".$row[11]."' name='sec__ID'>Moderate
+                    </form>";
+                    #  <td><button onClick=''>Moderate</button>";
+                    #<a href='enroll-mentee.php?cID=".$row[10]."&&secID=".$row[11]."'>
+                  } else if($test==1) {
                     $html_string .= "
                 <td>Moderating Section</button>";
-                  }
+              } else {
+                $html_string .= "
+            <td>N/A</button>";
+              }
                   }
 
                   $html_string .= "</td>";
