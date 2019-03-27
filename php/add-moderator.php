ba65fe5fed2ddbb72413c4385f3390ce70b7ac21
mmoschella <m_moschella@massbay.edu>
2019-03-27 11:30:41 -0400

    Testing to see that this works

--- php/add-moderator.php
+++ php/add-moderator.php
@@ -9,7 +9,6 @@
     $c_id = $_GET['c__ID'];
     $sec_id = $_GET['sec__ID'];
 
-    echo("<h1>$c_id</h1>");
 
     $get_student_info_query = "INSERT INTO Moderates VALUES ({$sec_id}, {$c_id}, {$active_id});";
     $result2 = mysqli_query($myconnection, $get_student_info_query) or die ('Query failed: ' . mysqli_error($myconnection));
